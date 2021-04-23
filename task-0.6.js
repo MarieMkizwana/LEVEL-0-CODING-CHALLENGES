32cfe35d218b74be9905f6c268c458ae86fe27f8
Violet Tshilowa <violet@ontax.co.za>
2021-04-23 12:00:15 +0200

    sixth task added

---
+++ task-0.6.js
@@ -0,0 +1,14 @@
+function maximum(num1, num2, num3){
+    if(num1 > num2){
+        return num1;
+    }
+    else if(num2 > num3){
+        return num2;
+    }
+    else{
+        return num3;
+    }
+
+}
+
+console.log(maximum(8, 15, 55));
